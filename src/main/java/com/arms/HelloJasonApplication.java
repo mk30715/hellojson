5ef323a4a2cf50dce2f4732d1f11222a58c45d93
mk30715 <mikio.kakuyama@arms-asia.com>
2016-09-08 12:26:20 +0700

    first commit for HelloJasonProject

---
+++ src/main/java/com/arms/HelloJasonApplication.java
@@ -0,0 +1,12 @@
+package com.arms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJasonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJasonApplication.class, args);
+	}
+}
